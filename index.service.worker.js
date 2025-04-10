9873a1ca57c946547ad6edf0760719c0727d43a9
Gytis <djgytis231@gmail.com>
2025-04-10 23:00:28 +0300

    Testing VirtualKeyboard

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1744312267|1116768064';
+const CACHE_VERSION = '1744315008|447391114';
 /** @type {string} */
 const CACHE_PREFIX = 'Clarity Quest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
